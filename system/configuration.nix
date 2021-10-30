0092bc25925d6dba3b9843ed13301317c6f5e45b
risolij <risolij@gmail.com>
2021-10-30 16:16:02 -0700

    updating to try nix flakes

--- system/configuration.nix
+++ system/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./networking/networking.nix
+      ## ./networking/networking.nix
       ./services/services.nix
     ];
 
