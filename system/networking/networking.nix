6ed4a1db27f3c93c3d984ef8e878f2c6c86b13f8
risolij <risolij@gmail.com>
2021-10-30 16:03:35 -0700

    updating

---
+++ system/networking/networking.nix
@@ -0,0 +1,28 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  networking = {
+    hostName = "nixos";
+    extraHosts = ''
+      @CONTROL@ control
+      @NODE01@ node01
+      @NODE02@ node02'';
+    useDHCP = false;
+    interfaces.wlp0s20f3.useDHCP = true;
+    wireless = {
+      environmentFile = /home/req/keys/wireless.env;
+      enable = true;
+      interfaces = [ "wlp0s20f3" ];
+      networks = {
+        F41B4D = {
+          pskRaw = "@PSK_HOME@";
+          authProtocols = [ "WPA-PSK" ];
+        };
+      };
+    };
+  };
+}
