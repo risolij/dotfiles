86cb3d91e508b5aaea49fc04213a49cfcf30b16f
risolij <risolij@gmail.com>
2022-01-29 18:08:35 -0700

    just better

--- .config/nixpkgs/packages/betterlockscreen/betterlockscreen.nix
+++ .config/nixpkgs/packages/betterlockscreen/betterlockscreen.nix
@@ -1,16 +1,8 @@
 { pkgs, lib, config, ... }:
 
-## let cfg = config.services.betterlockscreen;
-## in
 {
   services.betterlockscreen = {
     enable = true;
     inactiveInterval = 20;
   };
-  ##services.screen-locker = {
-  ##  enable = true;
-  ##  inactiveInterval = 20;
-  ##  lockCmd = "${cfg.package}/bin/betterlockscreen --lock";
-  ##};
-
 }
