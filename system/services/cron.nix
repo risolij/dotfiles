744714054f6740d85a792a9e756c2b676e4f1004
risolij <risolij@gmail.com>
2022-01-29 20:23:59 -0700

    updating timeframe & removing OK

--- system/services/cron.nix
+++ system/services/cron.nix
@@ -5,7 +5,7 @@
     enable = true;
     systemCronJobs = [
       "0 0 * * *    req curl -X GET https://download.dnscrypt.info/blacklists/domains/mybase.txt > /tmp/blacklist.txt"
-      "0 */12 * * *  req clamscan -r /home/req > /home/req/logs/output.txt"
+      "0 */4 * * *  req clamscan -r /home/req -o > /home/req/logs/lastscan.txt"
     ];
   };
 }
