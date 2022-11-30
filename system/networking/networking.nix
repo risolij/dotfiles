29ffdb6a0e3e36a84042305d1aa6281518b44174
risolij <risolij@gmail.com>
2022-11-29 19:01:12 -0700

    networking

--- system/networking/networking.nix
+++ system/networking/networking.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -9,8 +5,8 @@
   ## nmcli device wifi list
   ## nmcli device wifi connect "wifi-name" password "password"
 
-  ## ethtool -K eth0 tso off
-  ## ethtool -K eth0 gso off
+  ## ethtool -k wlp0s20f3
+    ## tx-tcp-segmentation off
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="net", NAME=="wlp0s20f3", RUN+="${pkgs.ethtool}/bin/ethtool -K wlp0s20f3 tso off"
   '';
