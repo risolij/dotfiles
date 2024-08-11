58e4cb8588f1a95d8548c73d8b1b0a3218513646
risolij <risolij@gmail.com>
2024-08-11 10:00:08 -0700

    mouse settings

--- home-manager/programs/hyprland/default.nix
+++ home-manager/programs/hyprland/default.nix
@@ -26,11 +26,13 @@
       input = {
           kb_layout = "us";
           follow_mouse = 1;
+          accel_profile = "flat";
       
           touchpad = {
               natural_scroll = "yes";
               disable_while_typing = true;
               clickfinger_behavior = 1;
+              scroll_factor = 2.0;
           };
 
       
