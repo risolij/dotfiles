2eb7a09bf89e64f78e4d6651be860d5500fe2664
risolij <risolij@gmail.com>
2025-05-30 22:51:52 -0700

    updates to kernelModules

--- hosts/home/boot.nix
+++ hosts/home/boot.nix
@@ -7,7 +7,6 @@
         "xhci_pci"
         "nvme"
         "usb_storage"
-        "sd_mod"
         "ext4"
         "i8042"
         "vfat"
@@ -16,11 +15,19 @@
     blacklistedKernelModules = [ "iTCO_wdt" ];
     extraModprobeConfig = ''
       options iwlwifi power_save=0
-      options iwlmvm power_scheme=1
+      options iwlmvm power_scheme=0
       options snd slots=sof-hda-dsp
     '';
     extraModulePackages = [ ];
-    kernelModules = [ "kvm-intel" ]; 
+    kernelModules = [
+      "kvm-intel"
+      "mei_me"
+      "thunderbolt"
+      "btusb"
+      "wacom"
+      "intel-lpss"
+      "i915"
+    ];
     kernelParams = [
       "nowatchdog"
       "nmi_watchdog=0"
