fc3dbc4468167d7e843069073fccd1fa93c1c012
risolij <risolij@gmail.com>
2025-11-14 02:01:14 -0700

    nixos renamed items

--- nixos/services/basic.nix
+++ nixos/services/basic.nix
@@ -1,7 +1,8 @@
 { pkgs, lib, config, ... }:
 
 {
-  services.logind.lidSwitch = "suspend";
+  ## services.logind.lidSwitch = "suspend";
+  services.logind.settings.Login.HandleLidSwitch = "suspend";
   services.flatpak.enable = true;
   services.openssh.enable = true;
   services.fwupd.enable = true;
