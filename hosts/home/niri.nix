755f7fd837a9f410f8d2a7762afc5b911075ddb2
risolij <risolij@gmail.com>
2026-01-02 05:55:57 -0700

    adding sample niri settings

--- hosts/home/niri.nix
+++ hosts/home/niri.nix
@@ -1,4 +1,22 @@
 { inputs, pkgs, ... }: {
 
-  programs.niri.enable = true;
+  programs.niri = {
+    enable = true;
+    settings = {
+      focus-follows-mouse.enable = true;
+      input = {
+        mod-key = "Super";
+
+        mouse = {
+          accel-profile = "flat";
+          accel-speed = 2.0;
+        };
+      };
+
+      binds = {
+        "Mod+D".action.spawn = "rofi -show drun";
+        "Mod+Enter".action.spawn = "alacritty";
+      };
+    };
+  };
 }
