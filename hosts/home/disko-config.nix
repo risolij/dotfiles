d321ec2cff6a63de00b77915675b581015da5c31
risolij <risolij@gmail.com>
2026-01-11 00:07:34 -0700

    disko config with luks

--- hosts/home/disko-config.nix
+++ hosts/home/disko-config.nix
@@ -1,79 +1,70 @@
-# disko.nix - Lenovo C940, NixOS, impermanent root, persistent /nix + /persist, zswap
-{ config, lib, pkgs, ... }:
-
-let
-  disk = "/dev/nvme0n1";
-in
 {
-  disko.targetDisk = disk;
-
-  # Partition layout
-  disko.partitions = [
-    # EFI system partition
-    {
-      mountPoint = "/boot";
-      size = "512M";
-      fsType = "vfat";
-      type = "efi";
-    }
-
-    # LUKS container for root, /nix, /persist
-    {
-      mountPoint = "/";
-      fsType = "btrfs";
-      type = "luks";
-      luksName = "cryptroot";
-      subvolumes = [
-        # Ephemeral root
-        {
-          name = "@root";
-          mountPoint = "/";
-          tmpfs = true;
-          size = "remaining";
-        }
-
-        # Persistent Nix store
-        {
-          name = "@nix";
-          mountPoint = "/nix";
-          size = "50G";
-        }
-
-        # Persistent configs & selective home
-        {
-          name = "@persist";
-          mountPoint = "/persist";
-          size = "50G";
-        }
-      ];
-    }
-
-    # LUKS-encrypted swap partition for zswap
-    {
-      mountPoint = "swap";
-      fsType = "swap";
-      type = "luks";
-      luksName = "cryptswap";
-      size = "8G";
-    }
-  ];
-
-  # User definition (home mapped to /persist/home)
-  disko.users = [
-    {
-      name = "yourusername";
-      uid = 1000;
-      home = "/persist/home/yourusername";
-      shell = pkgs.zsh;
-    }
-  ];
-
-  # Enable LUKS for root and swap
-  disko.enableLuks = true;
-  disko.luks = {
-    luksName = "cryptroot";
-    device = "/dev/nvme0n1p2";
-    preLVM = true;
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/vdb";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                # disable settings.keyFile if you want to use interactive password entry
+                #passwordFile = "/tmp/secret.key"; # Interactive
+                settings = {
+                  allowDiscards = true;
+                  keyFile = "/tmp/secret.key";
+                };
+                additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/swap" = {
+                      mountpoint = "/.swapvol";
+                      swap.swapfile.size = "20M";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
   };
 }
-
